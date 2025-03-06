f09e484d84ae723928ff2cc78dc58e487aa0aae4
Tobias Taurian Viana <eutobias@gmail.com>
2025-03-06 14:47:46 -0300

    chore: work in progress

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+
+let deck = new Reveal({
+  plugins: [Markdown, RevealHighlight],
+  // view: "scroll",
+  transition: 'slide',
+  scrollProgress: true,
+});
+deck.initialize();
